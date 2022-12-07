c12b5bc1a3236b9f87da803ee3a1db170fb22102
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2022-12-07 10:05:56 +0900

    fix index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from 'next';
 
-import { Input } from '@/components/forms';
 import { Layout } from '@/components/layouts';
 import { useAuth } from '@/hooks/useAuth';
 
@@ -10,7 +9,6 @@ const Home: NextPage = () => {
   return (
     <Layout>
       <p>Top page</p>
-      <Input />
       <button onClick={() => logout()}>ログアウト</button>
     </Layout>
   );
