a6ec58f6d0b64ff0246ddfebc31a9e9328f9c759
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2022-12-09 08:40:51 +0900

    create TaskListItem component and usePretch hook

---
+++ src/components/elements/TaskListItem.tsx
@@ -0,0 +1,69 @@
+import { ActionIcon, Checkbox, Grid, Group, Paper, Text } from '@mantine/core';
+import { FC, useRef, useState } from 'react';
+import { TrashX } from 'tabler-icons-react';
+
+import { Task } from '@/types/tasks';
+
+type Props = {
+  taskListId: string;
+  task: Pick<Task, 'id' | 'title' | 'status'>;
+  deleteHandler: (taskId: string) => Promise<void>;
+  completeHandler: (params: Task) => Promise<void>;
+  isCreating: boolean;
+  usePrefetchTask: (taskListId: string, taskId: string) => () => void;
+};
+
+const TaskListItem: FC<Props> = (props) => {
+  const { taskListId, task, deleteHandler, completeHandler, isCreating, usePrefetchTask } = props;
+
+  const [isChecked, setIsChecked] = useState(task.status === 'completed');
+  const prefetched = useRef<boolean>();
+  const prefetchTask = usePrefetchTask(taskListId, task.id);
+
+  return (
+    <Paper
+      shadow="xs"
+      p="md"
+      onMouseEnter={() => {
+        if (!prefetched.current) {
+          prefetchTask();
+          prefetched.current = true;
+        }
+      }}
+    >
+      <Grid grow justify="space-between" align="center">
+        <Grid.Col span={1}>
+          <Checkbox
+            defaultChecked={isChecked}
+            onChange={() => {
+              completeHandler({
+                id: task.id,
+                title: task.title,
+                status: isChecked ? 'needsAction' : 'completed',
+              });
+              setIsChecked(!isChecked);
+            }}
+          />
+        </Grid.Col>
+        <Grid.Col span={10}>
+          <Text variant="link">{task.title}</Text>
+        </Grid.Col>
+        <Grid.Col span={1}>
+          <Group position="center">
+            <ActionIcon
+              title="delete"
+              onClick={() => deleteHandler(task.id)}
+              disabled={isCreating}
+              color="red"
+              variant="light"
+            >
+              <TrashX />
+            </ActionIcon>
+          </Group>
+        </Grid.Col>
+      </Grid>
+    </Paper>
+  );
+};
+
+export default TaskListItem;
