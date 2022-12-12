026d57ba2cac0beb5653ca1dea26d137d3d2f7e1
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2022-12-13 08:57:07 +0900

    fix index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,21 @@
 import type { NextPage } from 'next';
-import { signIn, signOut, useSession } from 'next-auth/react';
+import { signOut } from 'next-auth/react';
 
 import { Layout } from '@/components/layouts';
 import { useFetchTaskLists } from '@/features/tasks/hooks/useFetchTaskLists';
-import { useAuth } from '@/hooks/useAuth';
 
 const Home: NextPage = () => {
-  const { logout } = useAuth();
-  const { data: session } = useSession();
-
   const { data: taskLists, isLoading } = useFetchTaskLists();
 
   return (
-    // <Layout>
-    <>
+    <Layout>
       <p>Top page</p>
-      <button onClick={() => logout()}>ログアウト</button>
-      <button onClick={() => signIn()}>signin</button>
       <button onClick={() => signOut()}>signout</button>
-    </>
-    // </Layout>
+    </Layout>
   );
 };
 
 export default Home;
+
+// import { useAuth } from '@/hooks/useAuth';
+// const { logout } = useAuth();
