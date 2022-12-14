c3b2e1117fb64240f0e6eb28f8c5cbd960e7354f
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2022-12-14 14:35:38 +0900

    delete Top

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,11 @@
-import { Container, Loader } from '@mantine/core';
 import type { NextPage } from 'next';
-import { Suspense } from 'react';
 
 import { Layout } from '@/components/layouts';
-import { Top } from '@/features/tasks/components';
 
 const Home: NextPage = () => {
   return (
     <Layout>
-      <Top />
+      <div></div>
     </Layout>
   );
 };
