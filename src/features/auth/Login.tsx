e71b428681f96026c23dc3200a5a1fe33b174cf9
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2022-12-14 15:31:33 +0900

    fix Login page

--- src/features/auth/Login.tsx
+++ src/features/auth/Login.tsx
@@ -1,9 +1,20 @@
+import { Button, Container, createStyles, Flex } from '@mantine/core';
 import { useGoogleLogin } from '@react-oauth/google';
 import { FC } from 'react';
 
 import { useAuth } from '@/hooks/useAuth';
 
+const useStyles = createStyles(() => ({
+  wrapper: {
+    height: '100vh',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+}));
+
 const Login: FC = () => {
+  const { classes } = useStyles();
   const { login } = useAuth();
 
   const googleLogin = useGoogleLogin({
@@ -13,9 +24,11 @@ const Login: FC = () => {
   });
 
   return (
-    <>
-      <button onClick={() => googleLogin()}>login</button>
-    </>
+    <div className={classes.wrapper}>
+      <Button size="lg" onClick={() => googleLogin()}>
+        Login
+      </Button>
+    </div>
   );
 };
 
